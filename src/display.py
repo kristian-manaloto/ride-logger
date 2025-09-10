06fc64de0e1680535540c0a1b305430ea23f0409
kcm662@usask.ca <kcm662@usask.ca>
2025-09-09 22:00:25 -0600

    displaying route over map picture

--- src/display.py
+++ src/display.py
@@ -1,8 +1,19 @@
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objects as go
 
+def show_fig(df):
 
-def show_fig(df: pd.DataFrame):
-    fig_1 = px.scatter(df, x='longitude',y='latitude', template='plotly_dark')
+    route_trace = go.Scattermapbox(lat=df['latitude'],lon=df['longitude'],
+        mode='lines',line=dict(width=3, color='red'),name="Route"
+    )
 
-    fig_1.show()
+    fig = go.Figure(route_trace)
+
+    fig.update_layout(
+        mapbox=dict(style="open-street-map",
+            center=dict(lat=df['latitude'].mean(), lon=df['longitude'].mean()),
+            zoom=12
+        ),
+        margin={"r":0,"t":0,"l":0,"b":0}
+    )
+
+    fig.show()
