55ec41df7062b1287b16f2ec30b6ec91581fc33d
kcm662@usask.ca <kcm662@usask.ca>
2025-09-13 21:16:28 -0600

    interpolating points for smoother rider marker

--- src/display.py
+++ src/display.py
@@ -1,8 +1,32 @@
 import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+
+def interpolate_route(df, n_points=500):
+    df = df.copy()
+    distances = np.zeros(len(df))
+    for i in range(1, len(df)):
+        distances[i] = distances[i-1] + np.sqrt(
+            (df['latitude'].iloc[i] - df['latitude'].iloc[i-1])**2 +
+            (df['longitude'].iloc[i] - df['longitude'].iloc[i-1])**2
+        )
+
+    lat_interp_func = interp1d(distances, df['latitude'], kind='linear')
+    lon_interp_func = interp1d(distances, df['longitude'], kind='linear')
+    speed_interp_func = interp1d(distances, df['cspeed'], kind='linear')
+
+    dist_new = np.linspace(0, distances[-1], n_points)
+    df_interp = pd.DataFrame({
+        'latitude': lat_interp_func(dist_new),
+        'longitude': lon_interp_func(dist_new),
+        'cspeed': speed_interp_func(dist_new)
+    })
+    return df_interp
 
 
 def show_fig(df,stops):
-
+    df_interp = interpolate_route(df,500)
     #draw the route
     route_trace = go.Scattermapbox(lat=df['latitude'],lon=df['longitude'],
         mode='lines',line=dict(width=3, color='red'),name="Route"
@@ -33,13 +57,13 @@ def show_fig(df,stops):
     fig = go.Figure(data=[route_trace, stop_marker ,rider_marker])
 
     steps = []
-
-    for i in range(len(df)):
+    for i in range(len(df_interp)):
         step = dict(
-            method="restyle",  
-            args=[{"lat": [[df['latitude'].iloc[i]]], 
-                "lon": [[df['longitude'].iloc[i]]]}, 
-                [2]], 
+            method="restyle",
+            args=[{
+                "lat": [[df_interp['latitude'].iloc[i]]],
+                "lon": [[df_interp['longitude'].iloc[i]]]
+            }, [2]],
             label=str(i)
         )
         steps.append(step)
